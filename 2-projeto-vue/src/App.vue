292172f0ba68b7e2a9ac96d8e6433af714a5c548
uEriic <ueriic0@gmail.com>
2023-03-18 11:33:04 -0400

    More components knowlodge
    Dia 02: 18/03/23
     Entendendo o conceito de hierarquia de componentes.

--- 2-projeto-vue/src/App.vue
+++ 2-projeto-vue/src/App.vue
@@ -3,18 +3,21 @@
     <PrimeiroComponente />
     <p>O dado do app é: {{ testando }}</p>
     <LifeCycle />
+    <Pessoa />
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
 
   export default {
     name: "App",
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa
     },
     data() {
       return {
---
+++ 2-projeto-vue/src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+    <form action="">
+        <h2>Login</h2>
+        <inputText />
+        <inputText />
+        <Submit />
+    </form>
+</template>
+
+<script>
+import Submit from './form/Submit.vue'
+import inputText from './form/inputText.vue'
+
+export default {
+    name: 'Form',
+    components: {
+        inputText,
+        Submit
+    }
+}
+</script>
