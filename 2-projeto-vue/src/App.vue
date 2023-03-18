d2907d8ea5d289a71840dc5c5119bb1f2f8b1679
uEriic <ueriic0@gmail.com>
2023-03-17 21:46:51 -0400

    Initial commit

---
+++ 2-projeto-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <p>O dado do app é: {{ testando }}</p>
+    <LifeCycle />
+  </div>
+</template>
+
+<script>
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
+
+  export default {
+    name: "App",
+    components: {
+      PrimeiroComponente,
+      LifeCycle
+    },
+    data() {
+      return {
+        testando: "Teste"
+      }
+    }
+  }
+</script>
---
+++ 2-projeto-vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ name }} e sou {{ job }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PrimeiroComponente',
+    data() {
+        return {
+            name: "Eric",
+            job: "Programming Student"
+        }
+    }
+}
+</script>
